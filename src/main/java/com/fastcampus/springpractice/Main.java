02971f6c83e4ae2bba93f7e3520a40205bba25b0
sungink <iamsungink@gmail.com>
2024-08-21 16:49:03 +0900

    #1 Main application 수정

--- src/main/java/com/fastcampus/springpractice/Main.java
+++ src/main/java/com/fastcampus/springpractice/Main.java
@@ -1,16 +1,22 @@
-package com.fastcampus.springpractice;
+package com.fastcampus;
 
+import com.fastcampus.springpractice.config.Config;
 import com.fastcampus.springpractice.logic.JavaSort;
 import com.fastcampus.springpractice.logic.Sort;
+import com.fastcampus.springpractice.service.SortService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello world!");
 
-        Sort<String> sort = new JavaSort<>();
+        ApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+        SortService sortService = context.getBean(SortService.class);
 
-        System.out.println("[result]=" + sort.sort(Arrays.asList(args)));
+        System.out.println("[result] " + sortService.doSort(Arrays.asList(args)));
 
     }
 }
