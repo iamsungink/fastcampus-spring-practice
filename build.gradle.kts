92e2d4cb655e2cfd1b7a82ff7de4f3f846ac9eac
sungink <iamsungink@gmail.com>
2024-08-22 17:17:07 +0900

    #1 spring webmvc 의존 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("war")
 }
 
 group = "com.fastcampus"
@@ -10,13 +11,26 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework:spring-webmvc:5.3.19")
 
-    testImplementation("org.springframework:spring-test:5.3.8")
+    compileOnly("javax.servlet:javax.servlet-api:4.0.1")
+
+    implementation("org.springframework:spring-webmvc:5.3.39")
+
+    testImplementation("org.springframework:spring-test:5.3.39")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+val explodedDir = layout.buildDirectory.dir("libs/exploded").get().asFile
+
+val explodedWar by tasks.register<Copy>("explodedWar") {
+    into(explodedDir)
+    with(tasks.war.get())
+}
+tasks.war {
+    finalizedBy(explodedWar)
 }
